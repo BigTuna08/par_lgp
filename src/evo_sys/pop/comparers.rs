fa00578c7d3c4d9ac9aeac3ef6ab3a80b85ce406
Kyle Nickerson <kln870@mun.ca>
2018-05-01 16:42:22 -0230

    more compare methods

--- src/evo_sys/pop/comparers.rs
+++ src/evo_sys/pop/comparers.rs
@@ -14,6 +14,11 @@ impl ResultMap{
             3 => self.pen_big(new_prog, old_prog),
             4 => self.var_pen_small(new_prog, old_prog),
             5 => self.var_pen_big(new_prog, old_prog),
+            6 => self.var_pen_double(new_prog, old_prog),
+            7 => self.var_pen_bigger(new_prog, old_prog),
+            8 => self.var_pen_big_feats(new_prog, old_prog),
+            9 => self.var_pen_bigger_feats(new_prog, old_prog),
+            10 => self.var_pen_bigger_halftime(new_prog, old_prog),
             _ => panic!("Invalid compare method!! \n{:?}", self.config),
         }
     }
@@ -113,6 +118,74 @@ impl ResultMap{
         } else {
             (new_prog.test_fit.unwrap() - v*new_prog.get_effective_len(0) as f32,
              old_prog.test_fit.unwrap() - v*old_prog.get_effective_len(0) as f32)
+        };
+
+        if new == old {
+            return rand::thread_rng().gen_weighted_bool(params::evolution::REPLACE_EQ_FIT);
+        }
+        return new > old
+    }
+
+    fn var_pen_bigger(&self, new_prog: &Program, old_prog: &Program) -> bool{
+        let period = 500_000.0;
+        let mut v = self.recieved_count as f32 / period;
+        v = (v.sin() + 1.0) / params::dataset::N_SAMPLES as f32;
+        v *= 40.0;
+
+        let new = new_prog.test_fit.unwrap() - v*new_prog.get_effective_len(0) as f32;
+        let old = old_prog.test_fit.unwrap() - v*old_prog.get_effective_len(0) as f32;
+
+        if new == old {
+            return rand::thread_rng().gen_weighted_bool(params::evolution::REPLACE_EQ_FIT);
+        }
+        return new > old
+    }
+
+
+    fn var_pen_big_feats(&self, new_prog: &Program, old_prog: &Program) -> bool{
+        let period = 500_000.0;
+        let mut v = self.recieved_count as f32 / period;
+        v = (v.sin() + 1.0) / params::dataset::N_SAMPLES as f32;
+        v *= 10.0;
+
+        let new = new_prog.test_fit.unwrap() - v*new_prog.get_n_effective_feats(0) as f32;
+        let old = old_prog.test_fit.unwrap() - v*old_prog.get_n_effective_feats(0) as f32;
+
+        if new == old {
+            return rand::thread_rng().gen_weighted_bool(params::evolution::REPLACE_EQ_FIT);
+        }
+        return new > old
+    }
+
+
+    fn var_pen_bigger_feats(&self, new_prog: &Program, old_prog: &Program) -> bool{
+        let period = 500_000.0;
+        let mut v = self.recieved_count as f32 / period;
+        v = (v.sin() + 1.0) / params::dataset::N_SAMPLES as f32;
+        v *= 40.0;
+
+        let new = new_prog.test_fit.unwrap() - v*new_prog.get_n_effective_feats(0) as f32;
+        let old = old_prog.test_fit.unwrap() - v*old_prog.get_n_effective_feats(0) as f32;
+
+        if new == old {
+        return rand::thread_rng().gen_weighted_bool(params::evolution::REPLACE_EQ_FIT);
+        }
+        return new > old
+    }
+
+
+    fn var_pen_bigger_halftime(&self, new_prog: &Program, old_prog: &Program) -> bool{
+        let period = 500_000.0;
+        let mut v = self.recieved_count as f32 / period;
+        v = (v.sin() + 1.0) / params::dataset::N_SAMPLES as f32;
+        v *= 10.0;
+
+
+        let (new, old) = if ((self.recieved_count as f32 / period ) as u16 % 2)== 0{
+            (new_prog.test_fit.unwrap() - v*new_prog.get_n_effective_feats(0) as f32,
+             old_prog.test_fit.unwrap() - v*old_prog.get_n_effective_feats(0) as f32)
+        } else {
+            (new_prog.test_fit.unwrap(), old_prog.test_fit.unwrap() )
         };
 
         if new == old {
